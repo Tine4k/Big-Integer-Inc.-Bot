b34d82d6e2c97e145f4d6644a956a7ce04386265
Markus Baumann <mbauma30@gmail.com>
2023-03-11 22:53:13 +0100

    a

--- services/CommandHandler.cs
+++ services/CommandHandler.cs
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-namespace PfannenkuchenBot;
-using Discord;
-using Discord.WebSocket;
-using System.Reflection;
-static public class CommandHandler
-{
-    static CommandHandler()
-    {
-        StaticCommands = typeof(Command).GetMethods(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Static);
-        InstanceCommands = typeof(Command).GetMethods(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance);
-    }
-    public static Task HandleCommand(SocketMessage msg)
-    {
-        var socketmsg = msg as SocketUserMessage;
-        if (socketmsg == null) return Task.CompletedTask;
-        if (!(socketmsg.Content.StartsWith(Config.prefix)) || socketmsg.Author.IsBot || socketmsg.Content.Length == 1) return Task.CompletedTask;
-        string[] cmd = socketmsg.Content.Remove(0, Config.prefix.Length).Split(' ');
-        EvaluateCommand(cmd, socketmsg);
-        return Task.CompletedTask;
-    }
-    static void EvaluateCommand(string[] cmd, SocketUserMessage socketmsg)
-    {
-        foreach (MethodBase m in StaticCommands) if (ConvMsgToCmd(cmd[0]) == m.Name.ToLower()) { m.Invoke(null, new object[] { socketmsg.Channel }); return; }
-        foreach (MethodBase m in InstanceCommands) if (ConvMsgToCmd(cmd[0]) == m.Name.ToLower()) { m.Invoke(new Command(socketmsg), new object[] { }); return; } // debug m.Invoke(typeof(Command).GetConstructor(new Type[]{typeof(SocketMessage)}).Invoke(new Object[]{socketmsg}), new object[] { }); return;
-        Command.Unknown(socketmsg.Channel);
-    }
-    public static string ConvMsgToCmd(string msg) => Format.StripMarkDown(msg).ToLower();
-    static readonly MethodInfo[] StaticCommands;
-    static readonly MethodInfo[] InstanceCommands;
-=======
 namespace PfannenkuchenBot.Core;
 using Discord;
 using Discord.WebSocket;
@@ -58,5 +27,4 @@ public static class CommandHandler
     public static string ConvMsgToCmd(string msg) => Format.StripMarkDown(msg).ToLower();
     static readonly MethodInfo[] StaticCommands;
     static readonly MethodInfo[] InstanceCommands;
->>>>>>> master
 }
--- services/Program.cs
+++ services/Program.cs
@@ -1,61 +1,3 @@
-<<<<<<< HEAD
-﻿namespace PfannenkuchenBot;
-using Discord;
-using Discord.WebSocket;
-static class Program
-{
-    public static void Main(string[] args)
-    {
-        if (args.Length > 0)
-        {
-            if (args[0] == "debug")
-            {
-                Console.WriteLine("Debugging");
-                Program.DebugStartup().GetAwaiter().GetResult();
-            }
-            else if (args[0] == "test")
-            {
-                Test();
-            }
-        }
-        else Program.Startup().GetAwaiter().GetResult();
-    }
-    public static DiscordSocketClient client = new DiscordSocketClient(new DiscordSocketConfig()
-    {
-        GatewayIntents =
-        GatewayIntents.Guilds |
-        GatewayIntents.GuildEmojis |
-        GatewayIntents.GuildMessages |
-        GatewayIntents.GuildMessageReactions |
-        GatewayIntents.MessageContent
-    });
-    static async Task Startup()
-    {
-        client.Log += Log;
-        Program.client.MessageReceived += CommandHandler.HandleCommand;
-        await client.LoginAsync(TokenType.Bot, File.ReadAllText("services/token.txt"));
-        await client.StartAsync();
-        await Task.Delay(-1);
-    }
-    static Task Log(LogMessage msg)
-    {
-        Console.WriteLine(msg.ToString());
-        return Task.CompletedTask;
-    }
-    static async Task DebugStartup()
-    {
-        client.Log += Log;
-        Program.client.MessageReceived += CommandHandler.HandleCommand;
-        await client.LoginAsync(TokenType.Bot, File.ReadAllText("services/config/token.txt"));
-        await client.StartAsync();
-        // debug here
-        await Task.Delay(-1);
-    }
-    static void Test()
-    {
-        
-    }
-=======
 ﻿namespace PfannenkuchenBot.Core;
 using Discord;
 using Discord.WebSocket;
@@ -97,5 +39,4 @@ static class Program
         Console.WriteLine(msg.ToString());
         return Task.CompletedTask;
     }
->>>>>>> master
 }
