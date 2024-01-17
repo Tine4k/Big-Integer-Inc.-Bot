f6e5caa2149ae2be0793432509b6d7543418d68a
Tine4k <mbauma30@gmail.com>
2024-01-17 23:26:04 +0100

    fixed goofy months

--- content/GoofyMonths.cs
+++ content/GoofyMonths.cs
@@ -1,17 +1,21 @@
-using System.Runtime.CompilerServices;
-
 namespace PfannenkuchenBot.Commands.Content.GoofyMonths;
 public static class Challenges
 {
     static Challenges()
     {
         CurrentChallengeName = "No Nazi November";
+        CurrentChallengeSave = $"saves/nonazinovember.txt";
     }
-    public static string CurrentChallengeName {get;}
-    static string[] ChallengeParticipipants {get => File.ReadAllLines("saves/");}
-    public static bool SignUp(string username)
+    public static string CurrentChallengeName {get;} 
+    static string CurrentChallengeSave {get;} 
+    static string[] ChallengeParticipipants {get => File.ReadAllLines(CurrentChallengeSave);}
+    public static void Loose(string username)
     {
-        if (ChallengeParticipipants.Contains(username)) return false;
-        else return true;
+        File.WriteAllText(CurrentChallengeSave, $"\n{username}");
+    }
+    public static bool HasLost(string username)
+    {
+        if (ChallengeParticipipants.Contains(username)) return true;
+        else return false;
     }
 }
